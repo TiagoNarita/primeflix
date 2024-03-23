6816d2557c5ccf0398677b61bb24f4e57de47956
Tiago <tiagobnarita@gmail.com>
2024-03-23 17:52:03 -0300

    loadFilme and catch

--- src/pages/Filmes/index.js
+++ src/pages/Filmes/index.js
@@ -1,12 +1,35 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import api from "../../services/api";
 
 function Filme() {
   const { id } = useParams();
+  const [movieDetails, setMovieDetails] = useState([]);
 
+  useEffect(() => {
+    async function loadFilme() {
+      const response = await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "7d1270e307ce5543af39f6418b1334e9",
+            language: "en-US",
+          },
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch(() => {
+          console.log("Filme não encontrado");
+        });
+    }
+
+    loadFilme();
+  }, []);
+
+  console.log(movieDetails);
   return (
     <div>
-      <h1>Welcome to the movie details page</h1>
+      <h1>Acessing the movie {id}</h1>
     </div>
   );
 }
