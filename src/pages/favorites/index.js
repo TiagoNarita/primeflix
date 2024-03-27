113719905ebeae674c9df1818609d0293192073e
Tiago <tiagobnarita@gmail.com>
2024-03-27 00:02:14 -0300

    criating the favorites movies

--- src/pages/favorites/index.js
+++ src/pages/favorites/index.js
@@ -1,10 +1,18 @@
 import "./favorites.css";
-import { useEffect, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const resp = localStorage.getItem("@primeflix");
+    const favoritesMovies = JSON.parse(resp);
+    setFavorites(favoritesMovies);
+  });
+
   return (
     <div>
-      <h1>Tela Favoritos</h1>
+      <h1>My Movies</h1>
     </div>
   );
 }
